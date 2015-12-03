ea0522e3eb221aee528301a25454795c8700db3e
Trent Willis <twillis@linkedin.com>
2015-12-03 13:31:50 -0800

    Add more test cases

---
+++ tests/unit/multiple-tests-test.js
@@ -0,0 +1,13 @@
+import { module, test } from 'qunit';
+
+module('#2: Module With Multiple Tests');
+
+test('#1', function(assert) { assert.ok(true); });
+test('#2', function(assert) { assert.ok(true); });
+test('#3', function(assert) { assert.ok(true); });
+test('#4', function(assert) { assert.ok(true); });
+test('#5', function(assert) { assert.ok(true); });
+test('#6', function(assert) { assert.ok(true); });
+test('#7', function(assert) { assert.ok(true); });
+test('#8', function(assert) { assert.ok(true); });
+test('#9', function(assert) { assert.ok(true); });
