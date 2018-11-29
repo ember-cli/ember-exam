bf58893098e03bec4c39ec01452523935ff65962
Stefan Penner <stefan.penner@gmail.com>
2018-11-29 10:34:01 -0800

    Upgrades + Updates
    * ember-cli
    * ember-try
    * syntax / es6
    * test usage
    * test execa usage (soon library itself)

--- testem.js
+++ testem.js
@@ -1,7 +1,5 @@
-/* eslint-env node */
 module.exports = {
-  framework: 'qunit',
-  test_page: 'tests/index.html?hidepassed&derp=herp',
+  test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
     'Chrome'
@@ -10,14 +8,18 @@ module.exports = {
     'Chrome'
   ],
   browser_args: {
-    Chrome: [
-      // --no-sandbox is needed when running Chrome inside a container
-      process.env.TRAVIS ? '--no-sandbox' : null,
-      '--disable-gpu',
-      '--headless',
-      '--remote-debugging-port=9222',
-      '--window-size=1440,900'
-    ].filter(Boolean)
-  },
-  parallel: -1
+    Chrome: {
+      ci: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.CI ? '--no-sandbox' : null,
+        '--headless',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900'
+      ].filter(Boolean)
+    }
+  }
 };
