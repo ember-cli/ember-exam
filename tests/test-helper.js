123173eac44eb2be4c881aa0ef0f3218b82da89d
Travis Hoover <thoov7@gmail.com>
2021-02-16 15:45:34 -0800

    Revert "fixing qunit vs mocha dep ordering resolution"
    This reverts commit adf907e31bba7c67218e6ad46ab7f125a5483f6e.

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -5,7 +5,7 @@ import resolver from './helpers/resolver';
 import start from 'ember-exam/test-support/start';
 import { macroCondition, dependencySatisfies } from '@embroider/macros';
 
-const oppositeFramework = macroCondition(dependencySatisfies('ember-mocha', '*')) ? 'qunit': 'mocha';
+const oppositeFramework = macroCondition(dependencySatisfies('ember-qunit', '*')) ? 'mocha': 'qunit';
 
 Object.keys(require.entries).forEach((entry) => {
   if (entry.indexOf(oppositeFramework) !== -1) {
