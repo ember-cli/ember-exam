36e50ab48ab0bdfab30eeb6c78333453f17e2bf7
Travis Hoover <thoov7@gmail.com>
2021-02-16 13:20:13 -0800

    wrap qunit tests for eai

--- tests/unit/qunit/async-iterator-test.js
+++ tests/unit/qunit/async-iterator-test.js
@@ -1,191 +1,195 @@
 import AsyncIterator from 'ember-exam/test-support/-private/async-iterator';
-import { module, test } from 'qunit';
+import { macroCondition, dependencySatisfies, importSync } from '@embroider/macros';
 
-module('Unit | Qunit | async-iterator', {
-  beforeEach() {
-    this.testem = {
-      eventHandler: new Array(),
-      emit: function(event) {
-        const argsWithoutFirst = Array.prototype.slice.call(arguments, 1);
-        if (this.eventHandler && this.eventHandler[event]) {
-          let handlers = this.eventHandler[event];
-          for (let i = 0; i < handlers.length; i++) {
-            handlers[i].apply(this, argsWithoutFirst);
+if (macroCondition(dependencySatisfies('ember-qunit', '*'))) {
+  let { module, test } = importSync('qunit');
+
+  module('Unit | Qunit | async-iterator', {
+    beforeEach() {
+      this.testem = {
+        eventHandler: new Array(),
+        emit: function(event) {
+          const argsWithoutFirst = Array.prototype.slice.call(arguments, 1);
+          if (this.eventHandler && this.eventHandler[event]) {
+            let handlers = this.eventHandler[event];
+            for (let i = 0; i < handlers.length; i++) {
+              handlers[i].apply(this, argsWithoutFirst);
+            }
           }
-        }
-      },
-      on: function(event, callBack) {
-        if (!this.eventHandler) {
-          this.eventHandler = {};
-        }
-        if (!this.eventHandler[event]) {
-          this.eventHandler[event] = [];
-        }
-        this.eventHandler[event].push(callBack);
-      },
-      removeEventCallbacks: () => {}
-    };
-  }
-});
-
-test('should instantiate', function(assert) {
-  let iteratorOfPromises = new AsyncIterator(this.testem, {
-    request: 'next-module-request',
-    response: 'next-module-response'
+        },
+        on: function(event, callBack) {
+          if (!this.eventHandler) {
+            this.eventHandler = {};
+          }
+          if (!this.eventHandler[event]) {
+            this.eventHandler[event] = [];
+          }
+          this.eventHandler[event].push(callBack);
+        },
+        removeEventCallbacks: () => {}
+      };
+    }
   });
 
-  assert.deepEqual(iteratorOfPromises.done, false);
-  assert.deepEqual(typeof iteratorOfPromises.next, 'function');
-  assert.deepEqual(typeof iteratorOfPromises.dispose, 'function');
-});
-
-test('should get the value from response.', function(assert) {
-  assert.expect(1);
-  const done = assert.async();
-  this.testem.on('next-module-request', () => {
-    this.testem.emit('next-module-response', {
-      done: false,
-      value: 'a'
+  test('should instantiate', function(assert) {
+    let iteratorOfPromises = new AsyncIterator(this.testem, {
+      request: 'next-module-request',
+      response: 'next-module-response'
     });
+
+    assert.deepEqual(iteratorOfPromises.done, false);
+    assert.deepEqual(typeof iteratorOfPromises.next, 'function');
+    assert.deepEqual(typeof iteratorOfPromises.dispose, 'function');
   });
 
-  const iteratorOfPromises = new AsyncIterator(this.testem, {
-    request: 'next-module-request',
-    response: 'next-module-response'
-  });
-
-  iteratorOfPromises.next().then(result => {
-    assert.deepEqual('a', result.value);
-    done();
-  });
-});
-
-test('should iterate promises until there is no response.', function(assert) {
-  assert.expect(1);
-  const done = assert.async();
-  const testem = this.testem;
-  const responses = ['a', 'b', 'c'];
-
-  testem.on('next-module-request', () => {
-    testem.emit('next-module-response', {
-      done: responses.length === 0,
-      value: responses.shift()
+  test('should get the value from response.', function(assert) {
+    assert.expect(1);
+    const done = assert.async();
+    this.testem.on('next-module-request', () => {
+      this.testem.emit('next-module-response', {
+        done: false,
+        value: 'a'
+      });
     });
-  });
 
-  const iteratorOfPromises = new AsyncIterator(testem, {
-    request: 'next-module-request',
-    response: 'next-module-response'
-  });
+    const iteratorOfPromises = new AsyncIterator(this.testem, {
+      request: 'next-module-request',
+      response: 'next-module-response'
+    });
 
-  let values = [];
-
-  iteratorOfPromises
-    .next()
-    .then(res => {
-      values.push(res.value);
-      return iteratorOfPromises.next();
-    })
-    .then(res => {
-      values.push(res.value);
-      return iteratorOfPromises.next();
-    })
-    .then(res => {
-      values.push(res.value);
-      assert.deepEqual(values, ['a', 'b', 'c']);
+    iteratorOfPromises.next().then(result => {
+      assert.deepEqual('a', result.value);
       done();
     });
-});
-
-test('should return false after disposing', function(assert) {
-  const iteratorOfPromises = new AsyncIterator(this.testem, {
-    request: 'next-module-request',
-    response: 'next-module-response'
   });
 
-  iteratorOfPromises.dispose();
+  test('should iterate promises until there is no response.', function(assert) {
+    assert.expect(1);
+    const done = assert.async();
+    const testem = this.testem;
+    const responses = ['a', 'b', 'c'];
 
-  assert.deepEqual(iteratorOfPromises.done, true);
-});
-
-test('should dispose after iteration.', function(assert) {
-  assert.expect(4);
-  const done = assert.async();
-  const testem = this.testem;
-  const responses = ['a', 'b', 'c'];
-
-  testem.on('next-module-request', () => {
-    testem.emit('next-module-response', {
-      done: responses.length === 0,
-      value: responses.shift()
+    testem.on('next-module-request', () => {
+      testem.emit('next-module-response', {
+        done: responses.length === 0,
+        value: responses.shift()
+      });
     });
+
+    const iteratorOfPromises = new AsyncIterator(testem, {
+      request: 'next-module-request',
+      response: 'next-module-response'
+    });
+
+    let values = [];
+
+    iteratorOfPromises
+      .next()
+      .then(res => {
+        values.push(res.value);
+        return iteratorOfPromises.next();
+      })
+      .then(res => {
+        values.push(res.value);
+        return iteratorOfPromises.next();
+      })
+      .then(res => {
+        values.push(res.value);
+        assert.deepEqual(values, ['a', 'b', 'c']);
+        done();
+      });
   });
 
-  const iteratorOfPromises = new AsyncIterator(testem, {
-    request: 'next-module-request',
-    response: 'next-module-response'
+  test('should return false after disposing', function(assert) {
+    const iteratorOfPromises = new AsyncIterator(this.testem, {
+      request: 'next-module-request',
+      response: 'next-module-response'
+    });
+
+    iteratorOfPromises.dispose();
+
+    assert.deepEqual(iteratorOfPromises.done, true);
   });
 
-  iteratorOfPromises
-    .next()
-    .then(res => {
-      assert.deepEqual(res.done, false);
-      return iteratorOfPromises.next();
-    })
-    .then(res => {
-      assert.deepEqual(res.done, false);
-      return iteratorOfPromises.next();
-    })
-    .then(res => {
-      assert.deepEqual(res.done, false);
-      return iteratorOfPromises.next();
-    })
-    .then(res => {
+  test('should dispose after iteration.', function(assert) {
+    assert.expect(4);
+    const done = assert.async();
+    const testem = this.testem;
+    const responses = ['a', 'b', 'c'];
+
+    testem.on('next-module-request', () => {
+      testem.emit('next-module-response', {
+        done: responses.length === 0,
+        value: responses.shift()
+      });
+    });
+
+    const iteratorOfPromises = new AsyncIterator(testem, {
+      request: 'next-module-request',
+      response: 'next-module-response'
+    });
+
+    iteratorOfPromises
+      .next()
+      .then(res => {
+        assert.deepEqual(res.done, false);
+        return iteratorOfPromises.next();
+      })
+      .then(res => {
+        assert.deepEqual(res.done, false);
+        return iteratorOfPromises.next();
+      })
+      .then(res => {
+        assert.deepEqual(res.done, false);
+        return iteratorOfPromises.next();
+      })
+      .then(res => {
+        assert.deepEqual(res.done, true);
+        done();
+      });
+  });
+
+  test('should resolve with iterator finishing if request is not handled within 2s', function(assert) {
+    assert.expect(1);
+    const iteratorOfPromises = new AsyncIterator(this.testem, {
+      request: 'next-module-request',
+      response: 'next-module-response',
+      timeout: 2
+    });
+
+    return iteratorOfPromises.next().then(res => {
       assert.deepEqual(res.done, true);
-      done();
     });
-});
-
-test('should resolve with iterator finishing if request is not handled within 2s', function(assert) {
-  assert.expect(1);
-  const iteratorOfPromises = new AsyncIterator(this.testem, {
-    request: 'next-module-request',
-    response: 'next-module-response',
-    timeout: 2
   });
 
-  return iteratorOfPromises.next().then(res => {
-    assert.deepEqual(res.done, true);
-  });
-});
+  test('should resolve a timeout error if request is not handled within 2s when emberExamExitOnError is true', function(assert) {
+    assert.expect(1);
+    const iteratorOfPromises = new AsyncIterator(this.testem, {
+      request: 'next-module-request',
+      response: 'next-module-response',
+      timeout: 2,
+      emberExamExitOnError: true,
+    });
 
-test('should resolve a timeout error if request is not handled within 2s when emberExamExitOnError is true', function(assert) {
-  assert.expect(1);
-  const iteratorOfPromises = new AsyncIterator(this.testem, {
-    request: 'next-module-request',
-    response: 'next-module-response',
-    timeout: 2,
-    emberExamExitOnError: true,
+    return iteratorOfPromises.next().then(() => {
+      assert.ok(false, 'Promise should not resolve, expecting reject');
+    },err => {
+      assert.deepEqual(
+        err.message,
+        'EmberExam: Promise timed out after 2 s while waiting for response for next-module-request'
+      );
+    });
   });
 
-  return iteratorOfPromises.next().then(() => {
-    assert.ok(false, 'Promise should not resolve, expecting reject');
-  },err => {
-    assert.deepEqual(
-      err.message,
-      'EmberExam: Promise timed out after 2 s while waiting for response for next-module-request'
+  test('should throw an error if handleResponse is invoked while not waiting for a response', function(assert) {
+    const iteratorOfPromises = new AsyncIterator(this.testem, {
+      request: 'next-module-request',
+      response: 'next-module-response'
+    });
+
+    assert.throws(
+      () => iteratorOfPromises.handleResponse({}),
+      /Was not expecting a response, but got a response/
     );
   });
-});
-
-test('should throw an error if handleResponse is invoked while not waiting for a response', function(assert) {
-  const iteratorOfPromises = new AsyncIterator(this.testem, {
-    request: 'next-module-request',
-    response: 'next-module-response'
-  });
-
-  assert.throws(
-    () => iteratorOfPromises.handleResponse({}),
-    /Was not expecting a response, but got a response/
-  );
-});
+}
--- tests/unit/qunit/multiple-tests-test.js
+++ tests/unit/qunit/multiple-tests-test.js
@@ -1,31 +1,35 @@
-import { module, test } from 'qunit';
+import { macroCondition, dependencySatisfies, importSync } from '@embroider/macros';
 
-module('Qunit | #2: Module With Multiple Tests');
+if (macroCondition(dependencySatisfies('ember-qunit', '*'))) {
+  let { module, test } = importSync('qunit');
 
-test('#1', function(assert) {
-  assert.ok(true);
-});
-test('#2', function(assert) {
-  assert.ok(true);
-});
-test('#3', function(assert) {
-  assert.ok(true);
-});
-test('#4', function(assert) {
-  assert.ok(true);
-});
-test('#5', function(assert) {
-  assert.ok(true);
-});
-test('#6', function(assert) {
-  assert.ok(true);
-});
-test('#7', function(assert) {
-  assert.ok(true);
-});
-test('#8', function(assert) {
-  assert.ok(true);
-});
-test('#9', function(assert) {
-  assert.ok(true);
-});
+  module('Qunit | #2: Module With Multiple Tests');
+
+  test('#1', function(assert) {
+    assert.ok(true);
+  });
+  test('#2', function(assert) {
+    assert.ok(true);
+  });
+  test('#3', function(assert) {
+    assert.ok(true);
+  });
+  test('#4', function(assert) {
+    assert.ok(true);
+  });
+  test('#5', function(assert) {
+    assert.ok(true);
+  });
+  test('#6', function(assert) {
+    assert.ok(true);
+  });
+  test('#7', function(assert) {
+    assert.ok(true);
+  });
+  test('#8', function(assert) {
+    assert.ok(true);
+  });
+  test('#9', function(assert) {
+    assert.ok(true);
+  });
+}
