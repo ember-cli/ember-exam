43b4f854e08b68087835e110cdbe053c022c713e
Travis Hoover <thoov7@gmail.com>
2021-02-16 10:59:36 -0800

    fix qunit not found

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,8 +3,10 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
+  const self = defaults.project.findAddonByName('ember-exam');
+  const autoImport = self.options.autoImport;
   let app = new EmberAddon(defaults, {
-    // Add options here
+    autoImport
   });
 
   const { maybeEmbroider } = require('@embroider/test-setup');
--- index.js
+++ index.js
@@ -2,10 +2,36 @@
 
 'use strict';
 
+const VersionChecker = require('ember-cli-version-checker');
+
 module.exports = {
   name: require('./package').name,
 
   includedCommands() {
     return require('./lib/commands');
+  },
+
+  init() {
+    this._super.init.apply(this, arguments);
+    let versionChecker = new VersionChecker(this.project);
+
+    const hasMagicallyProvidedQUnit = versionChecker
+      .for('ember-qunit')
+      .lt('5.0.0-beta.1');
+
+    // Ember-qunit < 5 provides an AMD shim for qunit but newer versions now use
+    // ember-auto-import to include qunit. This means that qunit is no
+    // longer available for addons (if the parent app is using ember-qunit > 5) to
+    // directly import under embroider unless they are using ember-auto-import
+    // themselves. This condidionally falls back to not using ember-auto-import
+    // when the parent app is providing qunit because without this we would double
+    // include qunit resulting in a runtime error (qunit detects if it as
+    // already be added to the window object and errors if so).
+    if (hasMagicallyProvidedQUnit) {
+      this.options = this.options || {};
+      this.options.autoImport = {
+        exclude: ['qunit'],
+      };
+    }
   }
 };
