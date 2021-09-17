c4df63f46ea5165d612ba1205e5f9d6c7bbdd898
Nathanael Dekeister <nathanael_dekeister@ultimatesoftware.com>
2021-09-17 16:47:21 +0200

    fix: ensure ember-mocha blueprints are generated

--- bin/install-test-framework.sh
+++ bin/install-test-framework.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 if [ "${TEST_FRAMEWORK}" = "ember-mocha" ]; then
-  yarn remove ember-qunit 2>/dev/null && echo "n" | ember install ember-mocha || true
+  yarn remove ember-qunit 2>/dev/null && echo "n" | ember install ember-mocha || echo "n" | ember g ember-mocha || true
 fi
