51faa469d3f0efa1b65da43f2cea061ef2c20246
Sergey Astapov <sergeastapov@gmail.com>
2021-10-13 16:41:12 -0400

    Run ember-cli-update to v3.28.3 and make Ember v4 scenarios pass

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,13 +1,13 @@
 import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import config from 'dummy/config/environment';
 
-const App = Application.extend({
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  Resolver,
-});
+class App extends Application {
+  modulePrefix = config.modulePrefix;
+  podModulePrefix = config.podModulePrefix;
+  Resolver = Resolver;
+}
 
 loadInitializers(App, config.modulePrefix);
 
