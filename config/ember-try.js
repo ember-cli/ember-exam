14647a1fb0dddb6de62672cd965af94850ffe1fa
Tobias Bieniek <tobias@bieniek.cloud>
2021-12-10 19:14:14 +0100

    Drop support for Ember 3.19 and below

--- config/ember-try.js
+++ config/ember-try.js
@@ -34,22 +34,6 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.12.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.16',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.16.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.20',
         npm: {
           devDependencies: {
