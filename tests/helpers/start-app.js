955d71d0784951e3e5709693cdbf4b5cf32e3af6
Andrey Fel <andrey.fel@retailnext.net>
2023-12-27 16:19:28 +0100

    Fix @ember/polyfills deprecation
    We can use Object.assign instead

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,11 +1,9 @@
 import Application from '../../app';
 import config from '../../config/environment';
-import { merge } from '@ember/polyfills';
 import { run } from '@ember/runloop';
 
 export default function startApp(attrs) {
-  let attributes = merge({}, config.APP);
-  attributes = merge(attributes, attrs); // use defaults, but you can override;
+  const attributes = Object.assign({}, config.APP, attrs); // use defaults, but you can override;
 
   return run(() => {
     const application = Application.create(attributes);
