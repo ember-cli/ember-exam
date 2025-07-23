6af9e32eae94d0c246d33f2509ede687bbcb8b55
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-23 13:58:48 -0400

    Fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,30 @@
-import { ember } from 'ember-eslint';
+import globals from "globals";
+import { ember } from "ember-eslint";
+import * as url from "url";
+
+// Needed until Node 20
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 export default [
-    ...ember.recommended(import.meta.dirname),
+  ...ember.recommended(dirname),
+  {
+    files: ["lib/**/*"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["node-tests/**/*"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.mocha,
+      },
+    },
+    rules: {
+      "ember/no-test-support-import": "off",
+    },
+  },
 ];
