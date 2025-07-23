2c3b5a002a3417e987e31efd961d733f3e72bc5a
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-23 11:23:58 -0400

    Sample tests

--- test-apps/vite-with-compat/tests/test-helper.js
+++ test-apps/vite-with-compat/tests/test-helper.js
@@ -3,13 +3,14 @@ import config from 'vite-with-compat/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
-import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
+import { setupEmberOnerrorValidation } from 'ember-qunit';
+import { start as startEmberExam } from 'ember-exam/test-support';
 
-export function start() {
+export function start({ availableModules }) {
   setApplication(Application.create(config.APP));
 
   setup(QUnit.assert);
   setupEmberOnerrorValidation();
 
-  qunitStart();
+  startEmberExam({ availableModules });
 }
