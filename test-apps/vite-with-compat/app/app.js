6d59fbed092f80973abe5101bf75bf931a051ca8
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-23 10:58:03 -0400

    Add vite test-app

---
+++ test-apps/vite-with-compat/app/app.js
@@ -0,0 +1,18 @@
+import Application from '@ember/application';
+import compatModules from '@embroider/virtual/compat-modules';
+import Resolver from 'ember-resolver';
+import loadInitializers from 'ember-load-initializers';
+import config from 'vite-with-compat/config/environment';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
+
+if (macroCondition(isDevelopingApp())) {
+  importSync('./deprecation-workflow');
+}
+
+export default class App extends Application {
+  modulePrefix = config.modulePrefix;
+  podModulePrefix = config.podModulePrefix;
+  Resolver = Resolver.withModules(compatModules);
+}
+
+loadInitializers(App, config.modulePrefix, compatModules);
---
+++ test-apps/vite-with-compat/tests/test-helper.js
@@ -0,0 +1,15 @@
+import Application from 'vite-with-compat/app';
+import config from 'vite-with-compat/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
+
+export function start() {
+  setApplication(Application.create(config.APP));
+
+  setup(QUnit.assert);
+  setupEmberOnerrorValidation();
+
+  qunitStart();
+}
