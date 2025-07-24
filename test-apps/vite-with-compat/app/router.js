1e7e0bf9d76d7b0e1d41af6b1c84f7ea0021e496
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-23 23:37:28 -0400

    Remove @embroider/router

--- test-apps/vite-with-compat/app/router.js
+++ test-apps/vite-with-compat/app/router.js
@@ -1,4 +1,4 @@
-import EmberRouter from '@embroider/router';
+import EmberRouter from '@ember/router';
 import config from 'vite-with-compat/config/environment';
 
 export default class Router extends EmberRouter {
