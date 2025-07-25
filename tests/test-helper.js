9c3fc5e10f185a5e681008d7d5e753c9b6d87347
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-25 16:00:33 -0400

    Was this it?

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,15 @@
 import { setResolver } from '@ember/test-helpers';
 import resolver from './helpers/resolver';
 import start from 'ember-exam/test-support/start';
+import { setupEmberOnerrorValidation } from 'ember-qunit';
+
+setupEmberOnerrorValidation();
+
+QUnit.moduleStart(({ name }) => console.group(name));
+QUnit.testStart(({ name }) => console.group(name));
+QUnit.testDone(() => console.groupEnd());
+QUnit.moduleDone(() => console.groupEnd());
+
 
 setResolver(resolver);
 start();
