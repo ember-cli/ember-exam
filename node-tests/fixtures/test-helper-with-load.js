104f4fbde1a2eaf89c43873496f1ece75f7e03b3
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-26 19:23:48 -0400

    Update

--- node-tests/fixtures/test-helper-with-load.js
+++ node-tests/fixtures/test-helper-with-load.js
@@ -2,10 +2,12 @@ import Application from 'dummy/app';
 import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import loadEmberExam from 'ember-exam/test-support/load';
+import { setupEmberOnerrorValidation } from 'ember-qunit';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
 
 loadEmberExam();
+setupEmberOnerrorValidation();
 
 start();
