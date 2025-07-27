ff11adb8d5d85be6ae1533f5965b9e2a8b76772d
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-27 13:26:05 -0400

    We need loadTests() from ember-qunit in AMD environments

--- node-tests/fixtures/test-helper-with-load.js
+++ node-tests/fixtures/test-helper-with-load.js
@@ -3,10 +3,12 @@ import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import loadEmberExam from 'ember-exam/test-support/load';
 import { start, setupEmberOnerrorValidation } from 'ember-qunit';
+import { loadTests } from 'ember-qunit/test-loader';
 
 setApplication(Application.create(config.APP));
 setupEmberOnerrorValidation();
 
 loadEmberExam();
 
+loadTests();
 start();
