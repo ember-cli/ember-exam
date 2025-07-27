5d1249cb5c57d9946cc151ab6d3ee632c0ab2e88
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-27 13:50:19 -0400

    hm

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,9 +3,7 @@ import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start as startEmberExam } from 'ember-exam/test-support';
 import { setupEmberOnerrorValidation } from 'ember-qunit';
-import { loadTests } from 'ember-qunit/test-loader';
 
 setApplication(Application.create(config.APP));
 setupEmberOnerrorValidation();
-loadTests();
 startEmberExam();
