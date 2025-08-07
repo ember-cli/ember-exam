2b5de0f580065b58209e0367f63e12e25c044803
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-08-07 10:56:00 -0400

    Update tests for split out apps

--- test-apps/embroider3-webpack/tests/test-helper.js
+++ test-apps/embroider3-webpack/tests/test-helper.js
@@ -1,14 +1,12 @@
 import Application from 'embroider3-webpack/app';
 import config from 'embroider3-webpack/config/environment';
-import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
-import { setup } from 'qunit-dom';
-import { loadTests } from 'ember-qunit/test-loader';
-import { start, setupEmberOnerrorValidation } from 'ember-qunit';
+import { setupEmberOnerrorValidation } from 'ember-qunit';
+import { start as startEmberExam } from 'ember-exam/test-support';
+import RSVP from 'rsvp';
+// RSVP.configure('async', (callback, arg) => queueMicrotask(() => callback(arg)));
+RSVP.configure('async', (callback, arg) => callback(arg));
 
 setApplication(Application.create(config.APP));
-
-setup(QUnit.assert);
 setupEmberOnerrorValidation();
-loadTests();
-start();
+startEmberExam();
