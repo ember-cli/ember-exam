079619169048611d271399d0c51b22cd952a5de0
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-08-08 19:14:08 -0400

    Delete test duplication and use symlinks instead

--- test-apps/broccoli/tests/unit/multiple-edge-cases-test.js
+++
@@ -1,7 +0,0 @@
-import { module, test } from 'qunit';
-
-module('#3: Module With Multiple Edge Case Tests', function () {
-  test('#1 RegExp test', function (assert) {
-    assert.ok(/derp/.test('derp'));
-  });
-});
