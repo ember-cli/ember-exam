4495b3673f2b3a50173aec8353ea77cb589bd1a1
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-08-26 17:01:16 -0400

    Remove temporary debugging code

--- test-apps/embroider3-webpack/tests/test-helper.js
+++ test-apps/embroider3-webpack/tests/test-helper.js
@@ -3,9 +3,6 @@ import config from 'embroider3-webpack/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { setupEmberOnerrorValidation } from 'ember-qunit';
 import { start as startEmberExam } from 'ember-exam/test-support';
-import RSVP from 'rsvp';
-// RSVP.configure('async', (callback, arg) => queueMicrotask(() => callback(arg)));
-RSVP.configure('async', (callback, arg) => callback(arg));
 
 setApplication(Application.create(config.APP));
 setupEmberOnerrorValidation();
